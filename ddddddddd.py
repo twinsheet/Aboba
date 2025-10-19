109a169b8968db9599f208d921d6895e6bfca50e
IT-Academy-Gomel <haury2020@gmail.com>
2025-10-19 11:46:39 +0300

    push

--- ddddddddd.py
+++ ddddddddd.py
@@ -1,11 +1,17 @@
 import random as r
 import string as s
 
-simvoly = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
-           'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-           'z', 'x', 'c', 'v', 'b', 'n', 'm', '#', '$', '*', '_',
-           'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-           'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-           'Z', 'X', 'C', 'V', 'B', 'N', 'M']
 
-def generator_password(length=8):
+def generate_password(length=8, use_uppercase=True, use_lowercase=True, use_digits=True, use_special=True):
+    characters = ' '
+    if use_uppercase:
+        characters += s.ascii_uppercase
+    if use_lowercase:
+        characters += s.ascii_lowercase
+    if use_digits:
+        characters += s.digits
+    if use_special:
+        characters += s.punctuation
+
+    if not characters:
+        raise ValueError('')
