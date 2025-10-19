d99425b921dff4066c4b7491df81d12bdea6c692
IT-Academy-Gomel <haury2020@gmail.com>
2025-10-19 12:27:27 +0300

    push

--- ddddddddd.py
+++ ddddddddd.py
@@ -1,4 +1,4 @@
-from PyQt6.QtSql import password
+from PyQt6.QtWidgets import (QApplication)
 
 from untitled_ui import Ui_QMainWindow
 import random as r
@@ -11,30 +11,29 @@ class MainWindow(Ui_QMainWindow):
         self.ui = Ui_QMainWindow()
         self.ui.setupUi(self)
 
-def generate_password(length=8, use_uppercase=True, use_lowercase=True, use_digits=True, use_special=True):
-    characters = ' '
-    if use_uppercase:
-        characters += s.ascii_uppercase
-    if use_lowercase:
-        characters += s.ascii_lowercase
-    if use_digits:
-        characters += s.digits
-    if use_special:
-        characters += s.punctuation
-
-    if not characters:
-        raise ValueError('')
-
-    password = ''.join(r.choice(characters) for _ in range(length))
-    return password
-
-if __name__ == '__main__':
-    try:
-        length = Ui_QMainWindow.self.spinBox()
-
-        password = generate_password(length=length)
+        self.length = self.ui.spinBox.setValue()
 
 
 
-    except:
-        pass
+    def generate_password(self, use_uppercase=True, use_lowercase=True, use_digits=True, use_special=True):
+        characters = ' '
+        if use_uppercase:
+            characters += s.ascii_uppercase
+        if use_lowercase:
+            characters += s.ascii_lowercase
+        if use_digits:
+            characters += s.digits
+        if use_special:
+            characters += s.punctuation
+
+        if not characters:
+            raise ValueError('')
+
+        password = ''.join(r.choice(characters) for _ in range(self.length))
+        return password
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
