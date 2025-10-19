3030f82172054d7665a3133eba9d5b2201a45c4e
IT-Academy-Gomel <haury2020@gmail.com>
2025-10-19 11:34:53 +0300

    2

---
+++ ddddddddd.py
@@ -0,0 +1,11 @@
+import random as r
+import string as s
+
+simvoly = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
+           'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+           'z', 'x', 'c', 'v', 'b', 'n', 'm', '#', '$', '*', '_',
+           'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+           'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+           'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+def generator_password(length=8):
